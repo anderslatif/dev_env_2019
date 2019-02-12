4dbd83d7e564c38669f3a497dbcaca5bd544ec8b
anders <al@mobilize-systems.com>
2019-02-12 11:30:08 +0100

    Added my photo, selection logic

--- 1._Our_Team/main.js
+++ 1._Our_Team/main.js
@@ -1,12 +1,50 @@
 const teamMembers = [
 {
     "name": "Anders Latif",
-    "url": "",
+    "url": "https://avatars3.githubusercontent.com/u/7512403?s=460&v=4",
     "skills": [{"name": "PHP", "level": 2}],
-    "background": "",
-    "motto": ""
+    "background": ""
 },
 {
-
+    "name": "Cosmin",
+    "url": "",
+    "skills": [{"name": "PHP", "level": 2}],
+    "background": "background"
 }
 ];
+
+let selected = -1;
+
+
+$('#0').click(() => {
+    if (selected === 0) {
+        selected = -1;
+        clearProfileFields();
+    } else {
+        selected = 0;
+        populateProfileFields(teamMembers[0]);
+    }
+});
+
+$('#1').click(() => {
+    if (selected === 1) {
+        selected = -1;
+        clearProfileFields();
+    } else {
+        selected = 1;
+        populateProfileFields(teamMembers[1]);
+    }
+});
+
+function populateProfileFields(person) {
+    const profile = $('.profile');
+    profile.append('<div class="name">Name: <span class="name">' + person.name + '</span></div>');
+
+    // <div class="name"></div>
+    // <div class="background"></div>
+    // <div class="skills"></div>
+}
+
+function clearProfileFields() {
+    $('.profile').empty();
+}

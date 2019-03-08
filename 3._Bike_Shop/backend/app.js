379a073eaf90f0b93dae9d75fca28d14604ae2cd
anders <al@mobilize-systems.com>
2019-03-08 14:06:09 +0100

    Wrote an example selenium test - not working

---
+++ 3._Bike_Shop/backend/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+
+/*app.listen('browser-test', async (req, res) => {
+    const browsers = ['firefox', 'chrome'];
+    await testBrowser(browsers);
+
+});
+
+async function testBrowser(browsers) {
+    let errors = false;
+    const {Builder, By, Key} = require('selenium-webdriver');
+
+    browsers.forEach(async browser => {
+        let driver = await new Builder().forBrowser(browser).build();
+        try {
+            await driver.get('localhost:3000');
+            await driver.findElement(By.name('.products__component')).sendKeys('webdriver', Key.RETURN);
+        } finally {
+            await driver.quit();
+        }
+    });
+}*/
+
+app.listen(3000, (err) => {
+    if (err) {
+        console.log("Error ", err);
+    } else {
+        console.log("App is running on port 3000");
+    }
+});

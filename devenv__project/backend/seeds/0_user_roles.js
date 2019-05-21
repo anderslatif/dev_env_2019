70b18707222c7132a825197b98984b97ec3fd7d6
anders <anderslatif@gmail.com>
2019-05-21 18:16:11 +0200

    Added the roles.json file

--- devenv__project/backend/seeds/0_user_roles.js
+++ devenv__project/backend/seeds/0_user_roles.js
@@ -1,16 +1,9 @@
+const roles = require('../../roles');
 
 exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
   return knex('user_roles').del()
     .then(() =>
       // Inserts seed entries
-      knex('user_roles').insert([
-        { id: 1, role_name: 'ADMIN' },
-        { id: 2, role_name: 'HR' },
-        { id: 3, role_name: 'SALES' },
-        { id: 4, role_name: 'GATE_OFFICER' },
-        { id: 5, role_name: 'WAREHOUSE_LOADER' },
-        { id: 6, role_name: 'WAREHOUSE_DISPATCHER' },
-        { id: 7, role_name: 'AUDITOR' },
-      ]));
+      knex('user_roles').insert(roles.roles));
 };

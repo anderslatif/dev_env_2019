931fa256ec87cccdc63da16538dcfbdc74970039
anders <anderslatif@gmail.com>
2019-05-29 15:49:41 +0200

    Added the GET users route in the backend

--- devenv__project/backend/routes/user.js
+++ devenv__project/backend/routes/user.js
@@ -36,6 +36,15 @@ exports.userRoutes = (app, db) => {
     }
   });
 
+  app.get('/users', async (req,res) => {
+    if (/* userRoleId === 1 || userRoleId === 4 */ true) {
+      const userArray = await db.User.query().column('name', 'email', 'country', 'phone', 'user_role_id').select();
+      res.send(userArray);
+    } else {
+      res.status(403).send();
+    }
+  });
+
   app.post('/user', (req, res) => {
     const {
       name, password, email, phone, country, role,
